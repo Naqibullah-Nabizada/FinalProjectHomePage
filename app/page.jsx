0edac4786fd5bddb12708e10c47f4a5fab132328
Naqibullah <naqibullah1400@gmail.com>
2023-06-05 12:31:52 +0430

    fix finance image routing

--- app/page.jsx
+++ app/page.jsx
@@ -6,14 +6,14 @@ export default function Home() {
     <>
       <header className="flex justify-between text-2xl my-3">
         <div className="mx-4">
-          <Image src={'/../public/images/logo-m.jpg'} alt="logo" width={100} height={100} />
+          <Image src={'/images/logo-m.jpg'} alt="logo" width={100} height={100} />
         </div>
         <div className="flex flex-col text-2xl justify-center items-center">
           <h1>وزارت تحصیلات عالی</h1>
           <h1 className="mt-2">ریاست پوهنتون کابل</h1>
         </div>
         <div className="mx-4">
-          <Image src={'/../public/images/logo-u.jpg'} alt="logo" width={100} height={100} />
+          <Image src={'/images/logo-u.jpg'} alt="logo" width={100} height={100} />
         </div>
       </header>
       <hr />
@@ -21,26 +21,27 @@ export default function Home() {
         <section className="flex justify-center my-5">
 
           <Link href={"./finance"} className="card p-2 mx-3 w-[22%]">
-            <Image src={'/../public/images/finance.jpg'} alt="logo" width={200} height={200}
+            <Image src={'/images/finance.jpg'} alt="logo" width={200} height={200}
               className="card-img-top h-[15rem]" />
             <hr />
             <p className='card-title text-center text-xl pt-3'>آمریت مالی و حسابی</p>
           </Link>
 
           <Link href={"./hr"} className="card p-2 mx-3 w-[22%]">
-            <Image src={'/../public/images/hr.png'} alt="logo" width={200} height={200}
-
+            <Image src={'/images/hr.png'} alt="logo" width={200} height={200}
               className="card-img-top h-[15rem]" />
             <hr />
             <h1 className='card-title text-center text-xl pt-3'>آمریت منابع بشری</h1>
           </Link>
+
           <Link href={"./supplies"} className="card p-2 mx-3 w-[22%]">
-            <Image src={'/../public/images/S.png'} alt="logo" width={200} height={200}
+            <Image src={'/images/S.png'} alt="logo" width={200} height={200}
               className="card-img-top h-[15rem]" />
             <hr />
             <p className='card-title text-center text-xl pt-3'>آمریت تهیه و تدارکات</p>
-            </Link>
-        </section>  
+          </Link>
+
+        </section>
       </main>
       <footer className="text-center mt-[5rem] mb-3">
         <h4 className="text-zinc-500">copyright {new Date().getFullYear()} &copy;</h4>
